8d7aec3e37d6c095665797ade2a08c1eb71abec4
Vikas Kushwaha <dev@vikas.rocks>
2025-04-21 07:57:42 +0530

    Deploying a test server for frontend

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		allowedHosts: [
+			'thekarigarstop.vikas.rocks',
+			'localhost',
+		],
+	},
   plugins: [react()],
   build: {
     outDir: 'dist',

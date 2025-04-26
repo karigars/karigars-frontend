bce45159da2d362b7ad4f9ae2d7727505a141ffe
Vikas Kushwaha <dev@vikas.rocks>
2025-04-26 22:43:41 +0530

    resetting hosts and ports

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	  server: {
+    allowedHosts: [
+      'thekarigarstop.vikas.rocks',
+      'localhost',
+    ],
+    port: 5150,
+  },
   plugins: [react()],
   build: {
     outDir: 'dist',
